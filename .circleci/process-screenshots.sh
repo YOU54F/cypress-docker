650e7a6cd8047f75730ebcd7956b026e6b6aea69
Yousaf Nabi <yousafn@gmail.com>
2018-11-25 18:55:08 +0000

    traverse video and screenshot folders and publish to slack

--- .circleci/process-screenshots.sh
+++ .circleci/process-screenshots.sh
@@ -1,9 +1,9 @@
 export REPORT_ARTEFACT_URL=https://circleci.com/api/v1.1/project/github/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/artifacts/0/root/app
 
-# Travese the video folder and pull out all videos
+# Travese the screenshots folder and pull out all screenshots
 # format the files so they can be injected into a slack message
 # as a link. Replace http:// with variable for artifact path
-for f in cypress/videos/{,**/,**/**/,**/**/**/}*.mp4; do
+for f in cypress/screenshots/{,**/,**/**/,**/**/**/}*.png; do
 
   # ignore empty dirs that we globbed that have * in the path
     regex='\*'
@@ -14,7 +14,7 @@ for f in cypress/videos/{,**/,**/**/,**/**/**/}*.mp4; do
     fi
   done
 
-# Post the list of video links to slack
+# Post the list of screenshots links to slack
 curl -X POST -H 'Content-type: application/json' \
 --data '{"text": "'"$attachment_ahrefs"'" }' \
 $SLACK_WEBHOOK_URL
