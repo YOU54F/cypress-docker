040ab0a70303e29cf8f0e3507736704a93384035
Yousaf Nabi <yousafn@gmail.com>
2018-11-25 22:15:53 +0000

    purposely broken test

--- cypress/integration/examples/github.spec.js
+++ cypress/integration/examples/github.spec.js
@@ -2,7 +2,7 @@ describe('Github Login', function () {
   context('Authisation', function () {
     it('Cannot sign up without entering details', function () {
       cy.visit('www.github.com')
-      cy.get('.mr-4 > .octicon').should('exist')
+      cy.get('.mr-4 > .octicon').should('not.exist')
       cy.get('.home-hero-signup > .btn-mktg').click()
       cy.get('#signup-form > .flash').contains('There were problems creating your account.')
     })
