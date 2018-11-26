bb7fdbc9f1cd03f431d33a9b5fe9237758139e1e
Yousaf Nabi <yousafn@gmail.com>
2018-11-26 01:41:49 +0000

    Reporting (#5)
    * adding has to junit report
    
    * adding overwrite false to mochaawesome
    
    * collate junit test stats
    
    * collate junit test stats
    
    * using xml report
    
    * using xml report
    
    * failing test
    
    * removed logging

--- .circleci/slack-alert.sh
+++ .circleci/slack-alert.sh
@@ -1,20 +1,27 @@
-# if [ "${CIRCLE_BRANCH}" == "circleci" ]; then
-# two reports here, access it with .json or .html
 export REPORT_LOCATION=~/app/cypress/reports/mocha/mochawesome    
+export REPORT_LOCATION_JUNIT=~/app/cypress/reports/junit    
 export VIDEO_LOCATION=~/app/cypress/videos/ 
 export SCREENSHOT_LOCATION=~/app/cypress/screenshots/ 
 export REPORT_ARTEFACT_URL=https://circleci.com/api/v1.1/project/github/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/artifacts/0
 export REPORT_ARTEFACT_LOCATION=${REPORT_ARTEFACT_URL}${REPORT_LOCATION}.html
 export VIDEO_ARTEFACT_LOCATION=${REPORT_ARTEFACT_URL}${VIDEO_LOCATION}
-totalTestsPassing=$(jq '.stats.passes' ${REPORT_LOCATION}.json )
-totalTestsFailing=$(jq '.stats.failures' ${REPORT_LOCATION}.json )
-totalTests=$(jq '.stats.tests' ${REPORT_LOCATION}.json )
-testDuration=$(jq '.stats.duration' ${REPORT_LOCATION}.json )
-export TOTAL_TESTS=$totalTests
-export TOTAL_TESTS_FAILING=$totalTestsFailing 
-export TOTAL_TESTS_PASSING=$totalTestsPassing 
-export TEST_DURATION=$testDuration 
+
+
 export GIT_COMMIT_URL=https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/commit/${CIRCLE_SHA1}
+
+
+      for report in $REPORT_LOCATION_JUNIT/*.xml; do
+            totalSuiteTestsFailing=$(cat $report | egrep -o 'failures="[^.]' -m1 | cut -d \" -f2)
+            totalSuiteTests=$(cat $report | egrep -o 'tests="[^.]' -m1 | cut -d \" -f2)
+            totalTestCount=$((totalSuiteTests+totalTestCount))
+            failingTestCount=$((totalSuiteTestsFailing+failingTestCount))
+            durationTestCount=$durationTestCount+$testSuiteDuration
+       done
+
+      export TOTAL_TESTS_PASSING=$((totalTestCount-failingTestCount))
+      export TOTAL_TESTS_FAILING=$failingTestCount 
+      export TOTAL_TESTS=$totalTestCount
+      export TEST_DURATION=$durationTestCount 
 
       # Travese the artefact folders and pull out all artefacts,
       # format the files so they can be injected into a slack message
@@ -46,7 +53,6 @@ export GIT_COMMIT_URL=https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PRO
 
 # if no tests, we error and we must send this back to slack instead of a false positive
       if [ -z "$TOTAL_TESTS" ]; then
-            echo 'build fail loop' &&
             curl -X POST -H 'Content-type: application/json' \
             --data '{"text":"'${CIRCLE_PROJECT_REPONAME}' test build failed.\nThis run was triggered by <'$GIT_COMMIT_URL'|'${CIRCLE_USERNAME}'>'"$pr_link"'","channel":"'$SLACK_API_CHANNEL'",
             "attachments":[{"color":"#ff0000","fallback":"Report available at '$REPORT_ARTEFACT_LOCATION'",
@@ -56,7 +62,6 @@ export GIT_COMMIT_URL=https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PRO
             $SLACK_WEBHOOK_URL 
 # if total tests failing is more than 0, publish failure to slack
       elif [ $TOTAL_TESTS_FAILING -gt 0 ]; then
-            echo 'test fail loop' &&
             curl -X POST -H 'Content-type: application/json' \
             --data '{"text":"'${CIRCLE_PROJECT_REPONAME}' test run failed.\nThis run was triggered by <'$GIT_COMMIT_URL'|'${CIRCLE_USERNAME}'>'"$pr_link"'","channel":"'$SLACK_API_CHANNEL'",
             "attachments":[{"color":"#ff0000","fallback":"Report available at '$REPORT_ARTEFACT_LOCATION'",
@@ -68,7 +73,6 @@ export GIT_COMMIT_URL=https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PRO
             $SLACK_WEBHOOK_URL 
 # else if total tests failing is equal to 0, publish success to slack
       elif [ $TOTAL_TESTS_FAILING -eq 0 ]; then
-            echo 'passing loop' &&
             curl -X POST -H 'Content-type: application/json' \
             --data '{"text":"'${CIRCLE_PROJECT_REPONAME}' test run passed.\nThis run was triggered by <'$GIT_COMMIT_URL'|'${CIRCLE_USERNAME}'>'"$pr_link"'","channel":"'$SLACK_API_CHANNEL'",
             "attachments":[{"color":"#36a64f","fallback":"Report available at '$REPORT_ARTEFACT_LOCATION'",
@@ -79,9 +83,3 @@ export GIT_COMMIT_URL=https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PRO
             $SLACK_WEBHOOK_URL     
       # closing if from slack reporting conditions    
       fi 
-
-# closing if from branch condition at top of script    
-# fi
-
-      
-  
